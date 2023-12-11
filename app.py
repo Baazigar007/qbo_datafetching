4b16803b7c7fcea77205d7474af4a6540fb7618c
Mridul Gupta <43546695+mridul278@users.noreply.github.com>
2023-12-12 01:01:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,34 @@
+import base64
+import mysql.connector
+import requests
+import time
+import os
+
 from quickbooks import QuickBooks
 from intuitlib.client import AuthClient
-from intuitlib.enums import Scopes
-from quickbooks.objects.customer import Customer
 from quickbooks.objects.invoice import Invoice
-from requests_oauthlib import OAuth2Session
 import uuid
-import csv
-import mysql.connector
-import schedule
-import time
-from datetime import datetime, timedelta
-import pandas as pd
-import requests
-import os
-import json
-import base64
 
+SECRET_DICT = {}
 
-client_id = "ABAIju7db2lIL1HqnR0wTRVKrKyrJkS8ZSrLHBnA52RAKvqY07"
-client_secret = "2usElguOgftbR3VTkox3RyPAGjPJRbapvREUfmE3"
-TOKENS_PATH = '/tmp/data.txt'  # Replace with your actual tokens path in Firestore
-# TOKENS_PATH = 'data.txt'  # Adjust the path as needed
-combined_string = f"{client_id}:{client_secret}"
-
-# Base64 encode the combined string
-authorization_basic = base64.b64encode(combined_string.encode()).decode()
-
-
-def load_tokens():
-    try:
-        with open(TOKENS_PATH, 'r') as file:
-            data = file.read().splitlines()
-            return {
-                'accessToken': data[0],
-                'refreshToken': data[1],
-                'authorizationBasic': {authorization_basic},
-            }
-    except FileNotFoundError:
-        return None
-
-def save_tokens(tokens):
-    with open(TOKENS_PATH, 'w') as file:
-        file.write(f"{tokens['accessToken']}\n")
-        file.write(f"{tokens['refreshToken']}\n")
-        file.write(f"{tokens['authorizationBasic']}\n")
-
+class TokenRefreshError(Exception):
+    pass
+    
+def load_tokens_from_heroku():
+    SECRET_DICT["access_token"] = os.environ.get("QBO_ACCESS_TOKEN")
+    SECRET_DICT["refresh_token"] = os.environ.get("QBO_REFRESH_TOKEN")
+    SECRET_DICT["mysql_host"] = os.environ.get("MYSQL_HOST")
+    SECRET_DICT["mysql_user"] = os.environ.get("MYSQL_USERNAME")
+    SECRET_DICT["mysql_pwd"] = os.environ.get("MYSQL_PWD")
+    SECRET_DICT["client_id"] = os.environ.get("CLIENT_ID")
+    SECRET_DICT["client_secret"] = os.environ.get("CLIENT_SECRET") 
+    SECRET_DICT["realm_id"] = os.environ.get("QBO_REALM_ID")
+    SECRET_DICT["redirect_uri"] = os.environ.get("REDIRECT_URI")
+    SECRET_DICT["authorization_basic"] = base64.b64encode("{client_id}:{client_secret}".format(
+        client_id=os.environ.get("CLIENT_ID"),
+        client_secret=os.environ.get("CLIENT_SECRET")
+    ).encode()).decode()
+    
 def refresh_tokens(refresh_token, authorization_basic):
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -64,139 +48,48 @@ def refresh_tokens(refresh_token, authorization_basic):
         json_data = response.json()
         refresh_token = json_data.get('refresh_token', '')
         access_token = json_data.get('access_token', '')
-        return {'refreshToken': refresh_token, 'accessToken': access_token}
+        if refresh_token and access_token:
+            SECRET_DICT['refresh_token'] = refresh_token
+            SECRET_DICT['access_token'] = access_token
     except requests.exceptions.RequestException as err:
-        print(f"Something went wrong: {err}")
-        return {'refreshToken': '', 'accessToken': ''}
-    
+        print(f"Something went wrong during token refresh: {err}")
+        raise TokenRefreshError("Token refresh failed")
 
 def refresh_token():
-    # Get Existing Tokens
-    tokens = load_tokens()
-    print('Refreshing Token')
+    try:
+        refresh_tokens(SECRET_DICT['refresh_token'], SECRET_DICT["authorization_basic"])
+    except TokenRefreshError:
+        raise
 
-    if not tokens:
-        # No tokens found
-        # Save an error message in the log or handle accordingly
-        print('No Tokens Found')
-    else:
-        data = tokens
-        if not data.get('accessToken') or not data.get('refreshToken') or not data.get('authorizationBasic'):
-            # Insufficient data in tokens
-            # Save an error message in the log or handle accordingly
-            print('Insufficient Data in Tokens')
-            return
-
-        new_tokens = refresh_tokens(data['refreshToken'], data['authorizationBasic'])
-
-        if not new_tokens['accessToken'] or not new_tokens['refreshToken']:
-            # Unable to fetch access token from QuickBooks
-            # Save an error message in the log or handle accordingly
-            print('Unable to Fetch Access Token from QB')
-        else:
-            # Successfully refreshed token
-            # Update tokens or handle accordingly
-            data['accessToken'] = new_tokens['accessToken']
-            data['refreshToken'] = new_tokens['refreshToken']
-            data['error'] = False
-            data['message'] = f"Successfully Fetched Access Token from QB @ {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
-            data['lastRun'] = int(datetime.now().timestamp())
-
-            save_tokens(data)
-            print('Refreshed Token Successfully')
-
-if __name__ == "__main__":
-    refresh_token()
-
-
-def process_invoices(access_token):
-
-# # Your client ID and client secret obtained from QuickBooks Developer Dashboard
-#     client_id = "ABAIju7db2lIL1HqnR0wTRVKrKyrJkS8ZSrLHBnA52RAKvqY07"
-#     client_secret = "2usElguOgftbR3VTkox3RyPAGjPJRbapvREUfmE3"
-#     redirect_uri = "https://developer.intuit.com/v2/OAuth2Playground/RedirectUrl"
-
-#     # The refresh token obtained during the initial OAuth 2.0 authorization
-#     refresh_token = "AB11710582767VCA74F5oxQXxkWkxl3KA3Xm5J2a8lJKqYdDpK"
-
-#     # QuickBooks API endpoints
-#     authorization_base_url = "https://appcenter.intuit.com/connect/oauth2"
-#     token_url = "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer"
-
-#     # Set up OAuth2 session
-#     oauth = OAuth2Session(client_id, redirect_uri=redirect_uri, token={'refresh_token': refresh_token})
-
-#     # Refresh the access token using the refresh token
-#     token = oauth.refresh_token(token_url, client_id=client_id, client_secret=client_secret)
-
-#     # Now you can use the new access token
-#     access_token = token['access_token']
-
-#     # Example: Print the access token
-#     print("Access Token:", access_token)
-
-
-    
+def process_invoices(connection: mysql.connector.connect):
     auth_client = AuthClient(
-            client_id='ABAIju7db2lIL1HqnR0wTRVKrKyrJkS8ZSrLHBnA52RAKvqY07',
-            client_secret='2usElguOgftbR3VTkox3RyPAGjPJRbapvREUfmE3',
-            access_token=access_token,  # If you do not pass this in, the Quickbooks client will call refresh and get a new access token. 
+            client_id=SECRET_DICT["client_id"],
+            client_secret=SECRET_DICT["client_secret"],
             environment='production',
-            redirect_uri='https://developer.intuit.com/v2/OAuth2Playground/RedirectUrl',
-
+            redirect_uri=SECRET_DICT["redirect_uri"]
         )
 
-
-    # # refresh_token = os.environ.get('QUICKBOOKS_REFRESH_TOKEN')
     client = QuickBooks(
             auth_client=auth_client,
-            # refresh_token=refresh_token,
-            # refresh_token="AB11710351767KwdQqbdUrqbBQ14zfD5mDoiJQ9GwlmwPL5wdj",
-            company_id='9130356041310986',
+            company_id=SECRET_DICT["realm_id"],
+            refresh_token=SECRET_DICT["refresh_token"]
         )
+    last_value_date_query = "SELECT max(date) from qbo_data;"
+    cursor = connection.cursor()
+    cursor.execute(last_value_date_query)
+    response = cursor.fetchall()
+    last_value_date = response[0][0]
 
-    
+    invoices = Invoice.query("SELECT * from {table_name} where TxnDate > '{last_value_date}'".format(
+        table_name = Invoice.qbo_object_name,
+        last_value_date = last_value_date
+    ), qb=client)
 
-    
-#     auth_client = AuthClient(
-#             client_id='ABAIju7db2lIL1HqnR0wTRVKrKyrJkS8ZSrLHBnA52RAKvqY07',
-#             client_secret='2usElguOgftbR3VTkox3RyPAGjPJRbapvREUfmE3',
-#             # access_token="",  # If you do not pass this in, the Quickbooks client will call refresh and get a new access token. 
-#             environment='production',
-#             redirect_uri='https://developer.intuit.com/v2/OAuth2Playground/RedirectUrl',
-#         )
-
-
-# # refresh_token = os.environ.get('QUICKBOOKS_REFRESH_TOKEN')
-
-#     client = QuickBooks(
-#             auth_client=auth_client,
-#             # refresh_token=refresh_token,
-#             refresh_token="AB11710265193s93KyZf6Q8Dv6vX02L7MYGEVEPgWBoL1B5cJm",
-#             company_id='9130356041310986',
-#         )
-
-#     client = QuickBooks(
-#         auth_client=auth_client,
-#         refresh_token="AB11710265193s93KyZf6Q8Dv6vX02L7MYGEVEPgWBoL1B5cJm",
-#         company_id='9130356041310986',
-#         minorversion=69
-#     )
-
-
-
-    # Sending the GET request
-    invoices = Invoice.all(qb=client,start_position="1", max_results=1000)
-    # print(invoices)
-    invoices_dict = []
-
-
-
-    # Getting School 
+    # Getting School
     def getSchool(customfield):
         for values in customfield:
          if values["Name"]=="School":
-            return values["StringValue"]    
+            return values["StringValue"]
         return ""
 
 
@@ -255,46 +148,27 @@ def process_invoices(access_token):
         return None  # Return None if no quantity is found
 
     # Getting Amount
-   
+
     def getAmount(Line):
-    
         amount = []
         for item in Line:
             if "Amount" not in item:
                 continue
-
             amount.append(item.get("Amount"))
-
-
         for price in amount:
-    #    print (price,amount)
-         return price
-    # Getting Description
+            return price
 
     def get_descriptions(Line):
         descriptions = []  # Initialize a list to store descriptions for each line
         for item in Line:
-            # print(item,"item")
-            # print(isinstance(item, dict),"Description" in item,item["Description"])
             if isinstance(item, dict) and "Description" in item and item["Description"] is not None:
-                # print("description",descriptions)
                 description = item["Description"]
-                # print(description)
                 descriptions.append(description)
-                # print(descriptions)
-        #return descriptions  
-    # Return the list of descriptions outside of the for loop
         for d in descriptions:
-            # print (d,descriptions)
             return d
-  
-    previous_date = datetime.now() - timedelta(days=1)
-    previous_date_str = previous_date.strftime("%Y-%m-%d")
     
-     # Manually filter invoices based on the transaction date
-    filtered_invoices = [invoice for invoice in invoices if invoice.to_dict()["TxnDate"] == previous_date_str]
-
-    for invoice in filtered_invoices:
+    dataset = []
+    for invoice in invoices:
         invoice_dict = invoice.to_dict()
         newmap = {}
         newmap["uuid"] = get_uuid()
@@ -308,168 +182,47 @@ def process_invoices(access_token):
         newmap["unitPrice"] = getUnitPrice(invoice_dict["Line"])
         newmap["descriptions"] = get_descriptions(invoice_dict["Line"])
 
-        invoices_dict.append(newmap)
+        dataset.append(newmap)
 
+    return dataset
 
-
-
-    csv_columns = ["uuid", "invoiceId", "date", "school", "sorority", "product", "amount", "productQty", "unitPrice", "descriptions"]
-
-    existing_data = []
-
-    def read_existing_data(file_path):
-        existing_data = []
-        try:
-            with open(file_path, 'r', newline='', encoding="utf-8") as input_file:
-                reader = csv.DictReader(input_file)
-                for row in reader:
-                    existing_data.append(row)
-        except FileNotFoundError:
-            pass  # If the file doesn't exist, there's no existing data
-        return existing_data
-
-    def write_data_to_csv(data, file_path, csv_columns):
-        with open(file_path, 'w', newline='', encoding="utf-8") as output_file:
-            writer = csv.DictWriter(output_file, fieldnames=csv_columns)
-            # writer.writeheader()
-            # writer.writerows(data)
-            if output_file.tell() == 0:  # Check if the file is empty
-                writer.writeheader()
-            writer.writerows(data)
-
-
-    existing_data = read_existing_data("outputdataNEW.csv")
-    existing_ids = set(entry["invoiceId"] for entry in existing_data)
-
-    # for invoice in invoices:
-    for invoice in filtered_invoices:
-        invoice_dict = invoice.to_dict()
-        invoice_id = invoice_dict["DocNumber"]
-
-        if invoice_id not in existing_ids:
-            school = getSchool(invoice_dict["CustomField"])
-            sorority = invoice_dict["CustomerRef"]["name"]
-            invoice_date = invoice_dict["TxnDate"]
-
-            for product_line in invoice_dict["Line"]:
-                # print ("PRODUCT LINE ", product_line,"AND INVOICE DICT",invoice_dict["Line"])
-                if "SalesItemLineDetail" in product_line:
-                    newmap = {}
-                    newmap["uuid"] = get_uuid()
-                    newmap["invoiceId"] = invoice_id
-                    newmap["date"] = invoice_date
-                    newmap["school"] = school
-                    newmap["sorority"] = sorority
-                    newmap["product"] = getProductName([product_line])
-                    newmap["amount"] = getAmount([product_line])
-                    newmap["productQty"] = getQty([product_line])
-                    newmap["unitPrice"] = getUnitPrice([product_line])
-                    newmap["descriptions"] = get_descriptions([product_line])
-                    existing_data.append(newmap)
-
-    write_data_to_csv(existing_data, "outputdataNEW.csv", csv_columns)
-    print("CSV file has been updated.")
-# Call the function to process the invoices
-# process_invoices()
-
-
-def refresh_token():
-    # Get Existing Tokens
-    tokens = load_tokens()
-    print('Refreshing Token')
-
-    if not tokens:
-        # No tokens found
-        # Save an error message in the log or handle accordingly
-        print('No Tokens Found')
-    else:
-        data = tokens
-        if not data.get('accessToken') or not data.get('refreshToken') or not data.get('authorizationBasic'):
-            # Insufficient data in tokens
-            # Save an error message in the log or handle accordingly
-            print('Insufficient Data in Tokens')
-            return
-
-        new_tokens = refresh_tokens(data['refreshToken'], data['authorizationBasic'])
-
-        if not new_tokens['accessToken'] or not new_tokens['refreshToken']:
-            # Unable to fetch access token from QuickBooks
-            # Save an error message in the log or handle accordingly
-            print('Unable to Fetch Access Token from QB')
-        else:
-            # Successfully refreshed token
-            # Update tokens or handle accordingly
-            data['accessToken'] = new_tokens['accessToken']
-            data['refreshToken'] = new_tokens['refreshToken']
-            data['error'] = False
-            data['message'] = f"Successfully Fetched Access Token from QB @ {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
-            data['lastRun'] = int(datetime.now().timestamp())
-
-            save_tokens(data)
-            print('Refreshed Token Successfully')
-
-            process_invoices(data['accessToken'])
-
-
-if __name__ == "__main__":
-    refresh_token()
-
-
-
-def import_csv_to_dbeaver_database_using_mysql(csv_file_path, database_connection):
+def export_data_from_csv_to_database(invoices, database_connection):
+    insert_query = "INSERT INTO qbo_data (uuid, invoiceId, date, school, sorority, product, amount, productQty, unitPrice, descriptions) VALUES "
     cursor = database_connection.cursor()
+    values = []
+    for invoice in invoices:
+        value_str = f"""('{invoice['uuid']}', '{invoice['invoiceId']}', '{invoice['date']}', 
+                        '{invoice['school']}', '{invoice['sorority']}', '{invoice['product']}', 
+                        {invoice['amount']}, {invoice['productQty']}, {invoice['unitPrice']}, '{invoice['descriptions']}')"""
+        values.append(value_str)
 
-    with open(csv_file_path, "r") as csvfile:
-        reader = csv.reader(csvfile)
-        next(reader, None) # Skip the header row
-
-        for row in reader:
-                # If not, insert the record
-                cursor.execute(
-                    "INSERT INTO qbo_data (uuid, invoiceId, date, school, sorority, product, amount, productQty, unitPrice, descriptions) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                    (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9])
-                )
-                database_connection.commit()
-
-    cursor.close()
+    insert_query += ', '.join(values)
+    cursor.execute(insert_query)
+    database_connection.commit()
     print("Data imported into the database")
 
-
-# Function to update the database periodically
 def update_database_periodically():
+    load_tokens_from_heroku()
     refresh_token()
+    connection = mysql.connector.connect(
+        host=SECRET_DICT["mysql_host"],
+        user=SECRET_DICT["mysql_user"],
+        password=SECRET_DICT["mysql_pwd"],
+        database="qbo"
+    )
+
+    invoices = process_invoices(connection)
 
     print("Starting database update...")
-    # Connect to the MySQL database
-    connection = mysql.connector.connect(
-        host="39q6t8.stackhero-network.com",
-        user="qb_sync",
-        password="qbo123",
-        database="qbo"    
-        
-    
-    )
-    # Specify the CSV file path
-    csv_file_path = "outputdataNEW.csv"
-
-
-    # Import data from the CSV file to the database, passing the processed_records set
-    import_csv_to_dbeaver_database_using_mysql(csv_file_path, connection)
-
-    # Close the database connection
+    if invoices:
+        print(f'Dumping {len(invoices)} records...')
+        export_data_from_csv_to_database(invoices, connection)
+        print("Data uploaded successfully!")
+    else:
+        print('No new data to upload!')
     connection.close()
 
-# Schedule the update function to run every day at 12 am 
-# schedule.every().day.at("17:00:00").do(update_database_periodically)
-# schedule.every(5).minutes.do(update_database_periodically)
-print("Done updating")
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
-
-
-
-
-
-
+if __name__ == "__main__":
+    while True:
+        update_database_periodically()
+        time.sleep(60*60*30) # waiting for 30 mins
